221580b359f3d4b0bf2b96a8b49995aaea999a3b
LakshitNirwan <lakshitnirwan5@gmail.com.com>
2026-06-24 11:17:38 +0530

    Add actionEffects

---
+++ src/data/actionEffects.ts
@@ -0,0 +1,17 @@
+export const ACTION_WORDS = [
+  "POW!",
+  "BAM!",
+  "WHAM!",
+  "BOOM!",
+  "ZAP!",
+  "KAPOW!",
+  "SMASH!",
+];
+
+export const ACTION_COLORS = [
+  "bg-comic-red",
+  "bg-comic-blue",
+  "bg-comic-yellow",
+  "bg-comic-purple",
+  "bg-comic-cyan",
+];
